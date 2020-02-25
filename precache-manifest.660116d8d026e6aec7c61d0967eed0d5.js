e8e1f78ca1c5dc8d1339b2454385b4276f594376
Marcin Hercog <mkp.hercog@gmail.com>
2020-02-25 20:19:09 +0100

    Updates

---
+++ precache-manifest.660116d8d026e6aec7c61d0967eed0d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a191df78771089f60af23bfbed6167e6",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "cc64aa307921b3562aec",
+    "url": "/weatherapp/static/css/main.77821ad2.chunk.css"
+  },
+  {
+    "revision": "44bab95695b23c29c462",
+    "url": "/weatherapp/static/js/2.efefa1b5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/weatherapp/static/js/2.efefa1b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc64aa307921b3562aec",
+    "url": "/weatherapp/static/js/main.25921cd8.chunk.js"
+  },
+  {
+    "revision": "e4f4b1bc41ffb8c106bc",
+    "url": "/weatherapp/static/js/runtime-main.23a29212.js"
+  },
+  {
+    "revision": "f2d726829e50a2d6862f1d5a6bf72337",
+    "url": "/weatherapp/static/media/greenVillage.f2d72682.jpg"
+  }
+]);
